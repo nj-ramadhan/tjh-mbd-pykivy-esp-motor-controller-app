c700301dc57b1791f01cc1d2e879f5f327a50634
taufiqasshydiqi <taufiqasshydiqi@gmail.com>
2023-12-03 15:55:16 +0700

    screen load alarm setup

--- main.py
+++ main.py
@@ -548,6 +548,119 @@ class ScreenParameterAlarm(MDBoxLayout):
 
     def nav_main_menu(self):
         self.screen_manager.current = 'screen_main_menu'
+    
+    def screen_load_alarm(self):
+        self.screen_manager.current = 'screen_load_alarm'
+
+class ScreenLoadAlarm(MDBoxLayout):
+    screen_manager = ObjectProperty(None)
+
+    def __init__(self, **kwargs):
+        super(ScreenLoadAlarm, self).__init__(**kwargs)
+        Clock.schedule_once(self.delayed_init)
+    
+    def degree_range(self, n): 
+        start = np.linspace(0,180,n+1, endpoint=True)[0:-1]
+        end = np.linspace(0,180,n+1, endpoint=True)[1::]
+        mid_points = start + ((end-start)/2.)
+        return np.c_[start, end], mid_points
+
+    def rot_text(self, ang): 
+        rotation = np.degrees(np.radians(ang) * np.pi / np.pi - np.radians(90))
+        return rotation
+
+    def gauge(self, labels, colors, arrow, title, ax):
+        N = len(labels)
+        # N=100
+        # colors[:20] = '#ED1C24'
+        # colors[:-20] = '#ED1C24'
+        # colors[19:-19] = '#006300'
+        
+        if arrow > N: 
+            raise Exception("\n\nThe category ({}) is greated than \
+            the length\nof the labels ({})".format(arrow, N))
+        """
+        if colors is a string, we assume it's a matplotlib colormap
+        and we discretize in N discrete colors 
+        """
+        if isinstance(colors, str):
+            cmap = cm.get_cmap(colors, N)
+            cmap = cmap(np.arange(N))
+            colors = cmap[::-1,:].tolist()
+        if isinstance(colors, list): 
+            if len(colors) == N:
+                colors = colors[::-1]
+            else: 
+                raise Exception("\n\nnumber of colors {} not equal \
+                to number of categories{}\n".format(len(colors), N))
+
+        """
+        begins the plotting
+        """
+        ang_range, mid_points = self.degree_range(N)
+        labels = labels[::-1]
+        
+        """
+        plots the sectors and the arcs
+        """
+
+        patches = []
+        for ang, c in zip(ang_range, colors): 
+            # sectors
+            patches.append(Wedge((0.,0.), .4, *ang, facecolor='#eeeeee', lw=2))
+            # arcs
+            patches.append(Wedge((0.,0.), .4, *ang, width=0.10, facecolor=c, lw=2, alpha=0.5))
+        
+        [ax.add_patch(p) for p in patches]
+
+        
+        """
+        set the labels (e.g. 'LOW','MEDIUM',...)
+        """
+        # for mid, lab in zip(mid_points, labels): 
+
+        #     ax.text(0.35 * np.cos(np.radians(mid)), 0.35 * np.sin(np.radians(mid)), lab, horizontalalignment='center', verticalalignment='center', fontsize=9, fontweight='bold', rotation = self.rot_text(mid))
+
+        """
+        set the bottom banner and the title
+        """
+        r = Rectangle((-0.4,-0.1),0.8,0.1, facecolor='#eeeeee', lw=2)
+        ax.add_patch(r)
+        
+        ax.text(0, -0.15, arrow, horizontalalignment='center', verticalalignment='center', fontsize=12)
+        ax.text(0, -0.3, title, horizontalalignment='center', verticalalignment='center', fontsize=10)
+
+        """
+        plots the arrow now
+        """
+        pos = mid_points[abs(arrow - N)]
+        
+        ax.arrow(0, 0, 0.225 * np.cos(np.radians(pos)), 0.225 * np.sin(np.radians(pos)), width=0.02, head_width=0.06, head_length=0.1, fc='k', ec='k')
+        
+        ax.add_patch(Circle((0, 0), radius=0.02, facecolor='k'))
+        ax.add_patch(Circle((0, 0), radius=0.01, facecolor='w', zorder=11))
+
+        """
+        removes frame and ticks, and makes axis equal and tight
+        """
+        ax.set_frame_on(False)
+        ax.axes.set_xticks([])
+        ax.axes.set_yticks([])
+        ax.axis('equal')
+        # remove this as well
+        # plt.tight_layout()
+
+    def delayed_init(self, dt):
+        self.fig0, self.ax0 = plt.subplots()
+        self.fig0.set_facecolor("#eeeeee")
+        self.fig0.set_alpha(0.0)
+
+        self.gauge(labels=['LOLO','LO','NORMAL','HI','HIHI'], colors=['#ED1C24', '#006300','#006300','#006300','#ED1C24'], arrow=4, title='Load [A]', ax=self.ax0)
+     
+        self.ids.load_gauge_layout.add_widget(FigureCanvasKivyAgg(self.fig0))
+
+    def nav_main_menu(self):
+        self.screen_manager.current = 'screen_main_menu'
 
 class ScreenScadaSecurity(MDBoxLayout):
     screen_manager = ObjectProperty(None)
